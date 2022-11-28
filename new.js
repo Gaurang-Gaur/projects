5e8c17d2560cc31bfd81c581b2ee1b272f5abf7e
Gaurang Gaur <>
2022-11-28 18:24:38 +0000

    Initial commit

---
+++ new.js
@@ -0,0 +1,10 @@
+let obj={
+  gaurang:100,
+  harry:100,
+lovish:100,
+  krish:38
+
+}
+for(let i=0;i<object.keys(obj)[i];i++){
+  console.log("The marks"+object.keys(obj)+"are"+obj[object.keys(marks)[i]])
+}
